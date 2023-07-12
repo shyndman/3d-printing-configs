fb3a62fa4a32fd89aca6068fda89bf639a69d383
Scott Hyndman <scotty.hyndman@gmail.com>
2023-07-12 04:23:54 -0400

    Write an extension to set pressure advance based on nozzle size

---
+++ klippy-extras/per_print_pressure_advance.py
@@ -0,0 +1,34 @@
+class PerPrintPressureAdvance:
+    def __init__(self, config):
+        self.printer = config.get_printer()
+        gcode = self.printer.lookup_object('gcode')
+        gcode.register_command("SET_PRINT_PRESSURE_ADVANCE",
+                                   self.cmd_SET_PRINT_PRESSURE_ADVANCE,
+                                   desc=self.cmd_SET_PRINT_PRESSURE_ADVANCE_help)
+
+    cmd_SET_PRINT_PRESSURE_ADVANCE_help = "Set pressure advance automatically, or based on provided nozzle sizes"
+    def cmd_SET_PRINT_PRESSURE_ADVANCE(self, gcmd):
+        nozzle_size = gcmd.get('NOZZLE', parser=str, default=None)
+        if nozzle_size == None:
+            gcmd.respond_info("SET_PRINT_PRESSURE_ADVANCE called, but no NOZZLE argument provided")
+            return
+
+        # Pressure advance
+        if nozzle_size == '0.2':
+            pa = 0.9
+        elif nozzle_size == '0.3':
+            pa = 0.8
+        elif nozzle_size == '0.4':
+            pa = 0.7
+        elif nozzle_size == '0.5':
+            pa = 0.6
+        elif nozzle_size == '0.6':
+            pa = 0.5
+
+        smoothing = 0.02
+
+        extruder = self.printer.lookup_object('toolhead').get_extruder()
+        extruder.extruder_stepper._set_pressure_advance(pa, smoothing)
+
+def load_config_prefix(config):
+    return PerPrintPressureAdvance(config)
