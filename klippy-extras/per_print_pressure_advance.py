eb3b6e15ef804fad6fe0a9c65755e6328ae6eac3
Scott Hyndman <scotty.hyndman@gmail.com>
2023-07-12 05:32:35 -0400

    Implement the first version of SET_PRINT_PRESSURE_ADVANCE

--- klippy-extras/per_print_pressure_advance.py
+++ klippy-extras/per_print_pressure_advance.py
@@ -1,34 +1,49 @@
 class PerPrintPressureAdvance:
     def __init__(self, config):
         self.printer = config.get_printer()
-        gcode = self.printer.lookup_object('gcode')
-        gcode.register_command("SET_PRINT_PRESSURE_ADVANCE",
-                                   self.cmd_SET_PRINT_PRESSURE_ADVANCE,
-                                   desc=self.cmd_SET_PRINT_PRESSURE_ADVANCE_help)
+        gcode = self.printer.lookup_object("gcode")
+        gcode.register_command(
+            "SET_PRINT_PRESSURE_ADVANCE",
+            self.cmd_SET_PRINT_PRESSURE_ADVANCE,
+            desc=self.cmd_SET_PRINT_PRESSURE_ADVANCE_help,
+        )
 
-    cmd_SET_PRINT_PRESSURE_ADVANCE_help = "Set pressure advance automatically, or based on provided nozzle sizes"
+    cmd_SET_PRINT_PRESSURE_ADVANCE_help = (
+        "Set pressure advance automatically, or based on provided nozzle sizes"
+    )
+
     def cmd_SET_PRINT_PRESSURE_ADVANCE(self, gcmd):
-        nozzle_size = gcmd.get('NOZZLE', parser=str, default=None)
+        nozzle_size = gcmd.get("NOZZLE", parser=str, default=None)
         if nozzle_size == None:
-            gcmd.respond_info("SET_PRINT_PRESSURE_ADVANCE called, but no NOZZLE argument provided")
+            gcmd.respond_info(
+                "SET_PRINT_PRESSURE_ADVANCE called, but no NOZZLE argument provided"
+            )
             return
 
         # Pressure advance
-        if nozzle_size == '0.2':
+        if nozzle_size == "0.2":
             pa = 0.9
-        elif nozzle_size == '0.3':
+        elif nozzle_size == "0.3":
             pa = 0.8
-        elif nozzle_size == '0.4':
+        elif nozzle_size == "0.4":
             pa = 0.7
-        elif nozzle_size == '0.5':
+        elif nozzle_size == "0.5":
             pa = 0.6
-        elif nozzle_size == '0.6':
+        elif nozzle_size == "0.6":
             pa = 0.5
+        else:
+            gcmd.respond_info(
+                "SET_PRINT_PRESSURE_ADVANCE called with unsupported NOZZLE {nozzle}".format(
+                    nozzle=nozzle_size
+                )
+            )
+            return
 
         smoothing = 0.02
 
-        extruder = self.printer.lookup_object('toolhead').get_extruder()
+        extruder = self.printer.lookup_object("toolhead").get_extruder()
         extruder.extruder_stepper._set_pressure_advance(pa, smoothing)
 
-def load_config_prefix(config):
+
+def load_config(config):
     return PerPrintPressureAdvance(config)
